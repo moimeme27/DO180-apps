5308157ed63e38d4c25a37726f69d6f6e0f46625
moimeme27 <91739679+moimeme27@users.noreply.github.com>
2022-06-15 11:13:12 +0200

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -2,4 +2,6 @@
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "Hello,This is a new page " . PHP_VERSION . "\n";
 print "on est a PARIS " . PHP_VERSION . "\n";
+print ""\n";
+print "BLABLA " . PHP_VERSION . "\n";
 ?>
